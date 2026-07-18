b69d257ac288a529ca544bb10f5f424a87998c65
sudojoyappan <joelchunkath@gmail.com>
2026-07-18 19:07:05 +0530

    class codes

--- sem3/classes/dsa/1.py
+++ sem3/classes/dsa/1.py
@@ -68,4 +68,22 @@ class SinglyLinkedList:
             if count>k:
                 slowP=slowP.next
         print(slowP.data)
+
+
+
+
+node1=Node(1)
+node2=Node(2)
+node3=Node(3)
+node4=Node(4)
+node5=Node(5)
+
+node1.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = node5
+ll=LinkedList()
+ll.head=node1
+
+
         
