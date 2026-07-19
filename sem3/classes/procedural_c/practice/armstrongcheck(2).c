9e2d80ab9d9d6a89779e83d473b8c59f126616f4
sudojoyappan <joelchunkath@gmail.com>
2026-07-19 10:54:43 +0530

    added time counter to armstrong number checker

--- sem3/classes/procedural_c/practice/armstrongcheck(2).c
+++ sem3/classes/procedural_c/practice/armstrongcheck(2).c
@@ -1,21 +1,19 @@
 #include <stdio.h>
-#include <conio.h>
-#include <math.h>
+#include <time.h>
 
-void main(){
-    
-    for(int n=1;n<=1000;n++){
+int main(){
+    clock_t start = clock();
+
+    for(int n=1;n<=100000;n++){
         int power=0;
         int temp=n;
         while(temp!=0){
             power+=1;
-            temp=temp/10;
+            temp/=10;
         }
 
         temp=n;
         int sum=0;
-        // int num=0;
-        // int c=1;
         while(temp!=0){
             int digits=temp%10;
             int p=1;
@@ -24,7 +22,13 @@ void main(){
             temp/=10;
         }
         if(sum==n){
-            printf("%d is an armstrong number.",n);
+            printf("%d is an armstrong number.\n", n);
         }
     }
+
+    clock_t end = clock();
+    double time_taken = (double)(end - start) / CLOCKS_PER_SEC;
+    printf("Time taken: %f seconds\n", time_taken);
+
+    return 0;
 }
